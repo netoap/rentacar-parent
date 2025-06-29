8cd8fd46f765944fc83dabf4512702800922fed7
netoap <alexanderneto5@gmail.com>
2025-06-29 09:18:19 +0200

    #35 Fix spec file imports and component exports to resolve Angular compiler errors Fix spec file imports and component exports to resolve Angular compiler errors

---
+++ rentacar-frontend/src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule, // 👈 esto resuelve ActivatedRoute, RouterLink, etc.
+        AppComponent
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+});
